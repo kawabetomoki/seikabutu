54fe644740fb1671bdfaae64a59042d8d969264c
kawabetomoki <st032067@m01.kyoto-kcg.ac.jp>
2023-04-07 20:24:32 +0900

    環境設定

--- database/migrations/2023_04_04_101634_create_comments_table.php
+++ database/migrations/2023_04_04_101634_create_comments_table.php
@@ -13,15 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
+            $table->string('title', 50);
+            $table->string('body');
             $table->timestamps();
-            $table->integer('flag')->default(0);
         });
     }
 
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 };
--- database/migrations/2023_04_04_101658_create_likes_table.php
+++ database/migrations/2023_04_04_101658_create_likes_table.php
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('likes', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
             $table->timestamps();
-            $table->integer('flag')->default(0);
         });
     }
 
@@ -32,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('likes');
     }
 };
