8fe457eee7d37a379423c734014deb7c9687aeee
kawabetomoki <st032067@m01.kyoto-kcg.ac.jp>
2023-04-10 21:10:44 +0900

    環境設定

--- database/migrations/2023_04_04_101228_create_posts_table.php
+++ database/migrations/2023_04_04_101228_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 50);
+            $table->string('title', 100);
             $table->string('summary', 100);            
             $table->string('body');
             $table->foreignId('user_id')->constrained();
