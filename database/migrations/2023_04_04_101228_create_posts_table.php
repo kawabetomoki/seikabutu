6482ae309b4fd614fb3f811d2995f4b8c80b64c1
kawabetomoki <st032067@m01.kyoto-kcg.ac.jp>
2023-04-14 17:33:10 +0900

    Blog削除(完成)

--- database/migrations/2023_04_04_101228_create_posts_table.php
+++ database/migrations/2023_04_04_101228_create_posts_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('body');
             $table->foreignId('user_id')->constrained();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
